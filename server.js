28fcff49157fd136fa486a96d393ba22c42069c3
Kuldeepkumhar-fs <kuldeepkumhar100@gmail.com>
2022-10-13 17:34:27 +0530

    Added new API of /user

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const user = []
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(user);
 });
 
 app.listen(port, () => {
